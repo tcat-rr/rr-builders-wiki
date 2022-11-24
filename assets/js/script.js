0219ff7ee2cb430189a645ad60d021c5e302b646
tcat-rr <taeterttot06@gmail.com>
2022-11-23 23:57:24 -0500

    updated theme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,19 @@
 const namefilter = document.getElementById('filter-name');
 const descfilter = document.getElementById('filter-desc');
 
-namefilter.addEventListener('change', filterTableByName);
-descfilter.addEventListener('change', filterTableByDesc);
+namefilter.addEventListener('change', filterTable);
+descfilter.addEventListener('change', filterTable);
 
 
-function filterTableByName(e) {
+function filterTable(e) {
     var filter = e.target.value.toLowerCase();
     
-    var chips = document.getElementsByClassName("chip-name");
-    for (var i = 0; i < chips.length; i++) {
-        if(chips[i].innerHTML.toLowerCase().includes(filter)) {
-            chips[i].parentElement.parentElement.style.display = '';
-        } else {
-            chips[i].parentElement.parentElement.style.display = 'none';
-        }
+    if(e.target.id == "filter-name") {
+        var chips = document.getElementsByClassName("chip-name");
+    } else {
+        var chips = document.getElementsByClassName("chip-desc");
     }
-}
-
-function filterTableByDesc(e) {
-    var filter = e.target.value.toLowerCase();
     
-    var chips = document.getElementsByClassName("chip-desc");
     for (var i = 0; i < chips.length; i++) {
         if(chips[i].innerHTML.toLowerCase().includes(filter)) {
             chips[i].parentElement.parentElement.style.display = '';
--- cv2.md
+++ cv2.md
@@ -10,22 +10,22 @@ js: script.js
 
 <table style="width:100%">
     <tr>
-        <th>Chip Name</th><th>Description</th>
+        <th style="width:200px">Chip Name</th><th>Description</th>
     </tr>
     <tr>
         <td>
             <input class="form-control input-block input-sm" id="filter-name" name="filter-name" type="text" placeholder="Filter by name..."/>
         </td>
         <td>
-            <input class="form-control input-block input-sm" id="filter-desc" type="text" placeholder="Filter by description..."/>
+            <input class="form-control input-block input-sm" id="filter-desc" name="filter-desc" type="text" placeholder="Filter by description..."/>
         </td>
     </tr>
     
     {% for node in site.data.cv2["Nodes"] %}
-    <tr id="{{ node[1]["ReadonlyChipName" }}">
-        <td>
+    <tr id="{{ node[1]["ReadonlyPaletteName" }}">
+        <td style="vertical-align: top;">
             <div class="chip-name">
-                {{ node[1]["ReadonlyChipName"] | replace: "<", "&lt;" }}
+                {{ node[1]["ReadonlyPaletteName"] | replace: "<", "&lt;" }}
             </div>
         </td>
         <td>
